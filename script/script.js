a848712f8eed5efffd7781e46507150b43dbe603
uetiari <arianeueti@gmail.com>
2018-09-26 17:00:36 -0300

    adicionado fases até fase 3

--- script/script.js
+++ script/script.js
@@ -1,24 +1,65 @@
-const botao = document.querySelector(".tarefa__botao");
-const botaoExcluir = document.querySelector(".botao__excluir");
+const botaoAdd = document.querySelector(".botao__enviar");
+const inputTarefa = document.getElementById("ListaTarefaInput");
+const tarefaCaixa = document.querySelector(".tarefa__caixa");
 
-const tarefaInput = document.getElementById("ListaTarefaInput");
-const boxTarefas = document.querySelector(".tarefa__caixa")
-
-
-botao.addEventListener("click", function(evento){
+botaoAdd.addEventListener("click", function(evento){
     evento.preventDefault();
   
-    const tarefasItens = document.createElement("div");
-    tarefasItens.className = "tarefa__itens";
-    tarefasItens.innerHTML = `<p> ${tarefaInput.value}</p>
-    <button class="botao__excluir">&times;</button>`
-
-    boxTarefas.appendChild(tarefasItens);
-})
-
-const botaoExcluir = document.querySelector(".botao__excluir");
-botaoExcluir.addEventListener("click", function(excluir){
-    excluir.preventDefault();
+    if(inputTarefa.value === undefined || inputTarefa.value === null || inputTarefa.value === "" || inputTarefa.value === " "){
+       tarefaImput.focus();
+         return false;
+       }
     
+    const itens = document.createElement("div");
+        itens.className = "tarefa__itens";
+        itens.innerHTML = `<p> ${inputTarefa.value}</p>`
+        tarefaCaixa.appendChild(itens);
+        itens.style.display = "flex";
+
+        inputTarefa.value = null;
+
+    itens.addEventListener("click", function(){  
+        
+        if(itens.classList.contains("tarefa__itens")){
+            itens.classList.remove("tarefa__itens");
+            itens.classList.add("tarefa__itens-checked");
+         } 
+          else{
+            itens.classList.remove("tarefa__itens-checked");
+            itens.classList.add("tarefa__itens");
+         }
+    })
+
+    const botaoExcluir = document.createElement("button");
+    botaoExcluir.className = "botao__excluir";
+    botaoExcluir.innerHTML = `<button class ="botao__excluir">&times;</button>`
+        itens.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click", function(excluir){
+        excluir.preventDefault();
+
+        itens.remove();
+        botaoExcluir.remove();
+    });
+// ------
+    const checkTudo = document.querySelector(".botao__selecionar-tudo");
+    const excluirTudo = document.querySelector(".botao__deletar-tudo");
+
+    checkTudo.addEventListener("click", function(checkall){
+        if(itens.classList.contains("tarefa__itens")){
+            itens.classList.remove("tarefa__itens");
+            itens.classList.add("tarefa__itens-checked");
+     }  
+    })
+
+    excluirTudo.addEventListener("click", function(clearall){
+        if(itens.classList.contains("tarefa__itens-checked")){
+            itens.remove();
+            botaoExcluir.remove();
+        } 
+    })
 
 })
+
+
+
